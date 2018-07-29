dddc3724572c5e86dd68a23f0082fae9742ccfe4
saikrishnadhulipalla <41868252+saikrishnadhulipalla@users.noreply.github.com>
2018-07-29 20:08:48 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner s = new Scanner (System.in);
+		int m = s.nextInt();
+		if ( m % 4 == 0 )
+		{
+			System.out.println("yes");
+		}
+		else
+		           System.out.println("no");
+	}
+}
